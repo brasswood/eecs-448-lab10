0bc18c7d29dbba392857e726fdf05244d7391a63
Andrew Riachi <ariachi@ku.edu>
2021-12-07 21:31:32 -0600

    formatting

--- e2/CreateUser.php
+++ e2/CreateUser.php
@@ -2,22 +2,22 @@
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
     ini_set('display_startup_errors', 1);
-
+    echo "<html><head><title>Create User</title></head><body>"
 
     function success() {
-        echo "<html><body><h1>Success</h1></body></html>";
+        echo "<h1>Success</h1>";
     }
 
     $mysqli = new mysqli("mysql.eecs.ku.edu", "***REMOVED***", "***REMOVED***", "***REMOVED***");
 
     echo "<html><body>";
     if ($mysqli->connect_errno) {
-        echo "<html><body><h1>Could not connect to DB</h1></body></html>";
+        echo "<h1>Could not connect to DB</h1></body></html>";
         exit();
     }
     // Ensure nonempty string
     if ($_POST["username"] == "") {
-        echo "<html><body><h1>Username cannot be empty</h1></body></html>";
+        echo "<h1>Username cannot be empty</h1></body></html>";
         exit();
     }
     // Prevent SQL injection by preparing and executing a statement
@@ -30,12 +30,13 @@
     // e.g. $_POST["username"] = "ha'); TRUNCATE TABLE Users; INSERT INTO Users (user_id) VALUES ('pwned"
     // $result = $mysqli->multi_query($query);
     if ($result == false) {
-        echo "<html><body><h1>Insertion failed</h1></body></html>";
-        echo $query;
+        echo "<h1>Insertion failed</h1>";
     }
     else {
         success();
     }
+
+    echo "</body></html>"
     $mysqli->close();
 
 ?>
